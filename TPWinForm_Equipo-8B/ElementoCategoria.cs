45634042284c2bae9f75895db8af8108072d0b47
FerRada82 <fer_rada82@hotmail.com>
2025-04-20 23:12:13 -0300

    Clases

---
+++ TPWinForm_Equipo-8B/ElementoCategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_Equipo_8B
+{
+    internal class ElementoCategoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ TPWinForm_Equipo-8B/ElementoMarca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_Equipo_8B
+{
+    internal class ElementoMarca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
